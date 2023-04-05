08afce2616904fa55db9605f8e55d35e7cdec003
FJohnHRoss <rossjohn936@gmail.com>
2023-04-04 18:22:11 -0700

    select update

--- client/src/components/NewTweet/NewTweet.tsx
+++ client/src/components/NewTweet/NewTweet.tsx
@@ -2,12 +2,27 @@ import React, { useState, useEffect } from "react";
 
 const NewTweet = () => {
   const [tweet, setTweet] = useState("");
+
+  const [audience, setAudience] = useState("Everyone")
+  const [showAudience, setShowAudience] = useState(false)
+
   const [circleAmount, setCircleAmount] = useState(0)
+
+  const [reply, setReply] = useState("Everyone")
+
 
   const handleText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.target.style.height = "1.5rem"
     e.target.style.height = e.target.scrollHeight + "px"
     setTweet(e.target.value);
+  }
+
+  const toggleAudience = () => {
+    if (showAudience) {
+      setShowAudience(false)
+    } else {
+      setShowAudience(true)
+    }
   }
 
 
@@ -18,16 +33,33 @@ const NewTweet = () => {
         text
       </label>
       <form action="" method="post" className="newTweet__form">
-        <select>
-          <option value="everyone">Everyone</option>
-          <option value="circle">
-            Twitter Circle
-            <p>
-              {circleAmount} <button type="button">Edit</button>
-            </p>
-          </option>
-        </select>
-        <textarea onChange={handleText} name="twee" className="newTweet__input object" placeholder="Whats's happening?" value={tweet} />
+        <button className="newTweet__form-audience-btn--1" type="button" onClick={toggleAudience}>
+          {audience} <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 inline">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+          </svg>
+        </button>
+        {
+          showAudience ?
+            <div className="newTweet__form-audience-container">
+              <h4>Choose audience</h4>
+              <div className="newTweet__form-audience-container-btn-1" onClick={() => {
+                setAudience("Everyone")
+                setShowAudience(false)
+              }}><h5>Everyone</h5></div>
+              <div className="newTweet__form-audience-container-btn-1" onClick={() => {
+                setAudience("Twitter Circle")
+                setShowAudience(false)
+              }}>
+                <h5>Twitter Circle</h5>
+                {circleAmount} <span>People</span> <button className="newTweet__form-audience-container-btn-2" type="button">
+                  <u>Edit</u>
+                </button>
+              </div>
+            </div>
+            :
+            ""
+        }
+        <textarea onChange={handleText} name="tweet" className="newTweet__input object" placeholder="Whats's happening?" value={tweet} />
         <div className="newTweet__form-buttons">
           <div className="newTweet__icons">
             <span>1</span>
@@ -41,7 +73,7 @@ const NewTweet = () => {
         </div>
       </form>
 
-    </section>
+    </section >
   )
 }
 
