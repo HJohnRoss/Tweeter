d166a937a1d552b818aa986711723431a702e60f
FJohnHRoss <rossjohn936@gmail.com>
2023-04-20 19:53:04 -0700

    update fixing files

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -5,10 +5,11 @@ import '../src/index.css'
 import './css/style.css'
 import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  // </React.StrictMode>
+);
--- reactapp/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    const appProxy = createProxyMiddleware({
-        target: 'https://localhost:7039'
-    });
-
-    app.use(appProxy);
-};
